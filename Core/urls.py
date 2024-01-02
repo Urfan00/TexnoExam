6ad7747077414345cbd78ff8575a314e213ee5f6
Urfan00 <urfanagazade75@gmail.com>
2024-01-02 18:40:42 +0400

    some issue

--- Core/urls.py
+++ Core/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import QuizView, RuleView
-
 
 
 urlpatterns = [
-    path('', RuleView.as_view(), name='index'),
-    path('quiz/', QuizView.as_view(), name='quiz')
+    
 ]
